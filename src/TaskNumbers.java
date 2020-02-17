adf5db557ecfd0b387933dda1115aeb195f75973
Tomasz Stepnik <tomasz.stepnik77@gmail.com>
2020-02-17 16:10:14 +0100

    resolve problem from request

--- src/TaskNumbers.java
+++ src/TaskNumbers.java
@@ -3,20 +3,18 @@ import java.util.Scanner;
 public class TaskNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double x = 0;
         System.out.println("Podaj liczbę:");
-        double usersNumber = sc.nextDouble();
+        int userNumber = sc.nextInt();
 
-        if (usersNumber > 0) {
-            double xx = 0;
-            while (x < usersNumber + 0.1) {
+        if (userNumber > 0) {
+            double x = 0;
+            while (x < userNumber + 0.1) {
                 System.out.printf("%.1f" + " ", x);
                 x += 0.1;
             }
-        } else if (usersNumber < 0) {
+        } else if (userNumber < 0) {
             double y = 0;
-            double yy = 0;
-            while (y >= usersNumber - 0.1) {
+            while (y >= userNumber - 0.1) {
                 System.out.printf("%.1f" + " ", y);
                 y -= 0.1;
             }
